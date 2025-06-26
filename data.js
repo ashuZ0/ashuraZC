2159e7e491bf8f9df1f73c9ad6a577b211ca99ae
ashuZ0 <zadeashutosh06@gmail.com>
2025-06-26 10:31:36 +0530

    Add files via upload

--- data.js
+++ data.js
@@ -1,26 +1,85 @@
 const mangaData = {
   1: {
-    title: "One Piece – Chapter 1",
+    title: "Solo Leveling – Chapter 1",
+    cover: "https://i.imgur.com/5Q3nZfB.jpg",
     pages: [
-      "https://i.imgur.com/dYcYQ7E.jpg",
-      "https://i.imgur.com/NKzC0ZK.jpg",
-      "https://i.imgur.com/5n33I2A.jpg"
+      "https://i.imgur.com/AmVae1X.jpg",
+      "https://i.imgur.com/VQewvBd.jpg",
+      "https://i.imgur.com/YRNGWgi.jpg"
     ]
   },
   2: {
-    title: "Naruto – Chapter 1",
+    title: "Omniscient Reader – Chapter 1",
+    cover: "https://i.imgur.com/0TgZuvn.jpg",
     pages: [
-      "https://i.imgur.com/w1uBtHn.jpg",
-      "https://i.imgur.com/wEBqkCQ.jpg",
-      "https://i.imgur.com/OHGVYRm.jpg"
+      "https://i.imgur.com/DzrTI3R.jpg",
+      "https://i.imgur.com/vV4UezA.jpg",
+      "https://i.imgur.com/qPFCZbZ.jpg"
     ]
   },
   3: {
-    title: "Bleach – Chapter 1",
+    title: "The Beginning After the End – Chapter 1",
+    cover: "https://i.imgur.com/j0Q8AgG.jpg",
     pages: [
-      "https://i.imgur.com/VZTGE6j.jpg",
-      "https://i.imgur.com/qPAuA0A.jpg",
-      "https://i.imgur.com/lA17kbI.jpg"
+      "https://i.imgur.com/6hhzAaD.jpg",
+      "https://i.imgur.com/qoxOe8u.jpg",
+      "https://i.imgur.com/ZQJTrmW.jpg"
+    ]
+  },
+  4: {
+    title: "Tower of God – Chapter 1",
+    cover: "https://i.imgur.com/zFGm8EO.jpg",
+    pages: [
+      "https://i.imgur.com/rrsKjMQ.jpg",
+      "https://i.imgur.com/wNoNycR.jpg"
+    ]
+  },
+  5: {
+    title: "Eleceed – Chapter 1",
+    cover: "https://i.imgur.com/KZMBiRk.jpg",
+    pages: [
+      "https://i.imgur.com/dHw9gRY.jpg",
+      "https://i.imgur.com/fFYbJmK.jpg"
+    ]
+  },
+  6: {
+    title: "Wind Breaker – Chapter 1",
+    cover: "https://i.imgur.com/mh7cWyT.jpg",
+    pages: [
+      "https://i.imgur.com/U3yZ37h.jpg",
+      "https://i.imgur.com/OQuJ1Sx.jpg"
+    ]
+  },
+  7: {
+    title: "Lookism – Chapter 1",
+    cover: "https://i.imgur.com/sVP8d0J.jpg",
+    pages: [
+      "https://i.imgur.com/cYoURxA.jpg",
+      "https://i.imgur.com/UmEfovW.jpg"
+    ]
+  },
+  8: {
+    title: "Dungeon Reset – Chapter 1",
+    cover: "https://i.imgur.com/A1iI8H3.jpg",
+    pages: [
+      "https://i.imgur.com/8INHzHZ.jpg",
+      "https://i.imgur.com/8vDn94I.jpg"
+    ]
+  },
+  9: {
+    title: "Mercenary Enrollment – Chapter 1",
+    cover: "https://i.imgur.com/Ymqc4Pb.jpg",
+    pages: [
+      "https://i.imgur.com/yrtChHt.jpg",
+      "https://i.imgur.com/9DLQGDU.jpg"
+    ]
+  },
+  10: {
+    title: "Nano Machine – Chapter 1",
+    cover: "https://i.imgur.com/8NsJv20.jpg",
+    pages: [
+      "https://i.imgur.com/yy74DgM.jpg",
+      "https://i.imgur.com/oMErnvP.jpg"
     ]
   }
 };
