d93e8bf64afa89f52ce06689462829864e42a6da
ashuZ0 <zadeashutosh06@gmail.com>
2025-06-26 10:08:17 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,26 @@
+const mangaData = {
+  1: {
+    title: "One Piece – Chapter 1",
+    pages: [
+      "https://i.imgur.com/dYcYQ7E.jpg",
+      "https://i.imgur.com/NKzC0ZK.jpg",
+      "https://i.imgur.com/5n33I2A.jpg"
+    ]
+  },
+  2: {
+    title: "Naruto – Chapter 1",
+    pages: [
+      "https://i.imgur.com/w1uBtHn.jpg",
+      "https://i.imgur.com/wEBqkCQ.jpg",
+      "https://i.imgur.com/OHGVYRm.jpg"
+    ]
+  },
+  3: {
+    title: "Bleach – Chapter 1",
+    pages: [
+      "https://i.imgur.com/VZTGE6j.jpg",
+      "https://i.imgur.com/qPAuA0A.jpg",
+      "https://i.imgur.com/lA17kbI.jpg"
+    ]
+  }
+};
